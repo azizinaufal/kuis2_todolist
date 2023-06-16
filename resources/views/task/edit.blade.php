084e84a087363a4aa81be0c764961f58feec505a
Moh. Naufal Azizi <naufalazizi.me@gmail.com>
2023-06-16 21:27:24 +0700

    Izin melapor,Kuis sudah selesai Pak

---
+++ resources/views/task/edit.blade.php
@@ -0,0 +1,39 @@
+@extends('layout.template')
+<!-- START FORM -->
+@section('konten')
+
+<form action='{{ url('task/'.$data->id) }}' method='post'>
+    @csrf
+    @method('PUT')
+    <div class="my-3 p-3 bg-body rounded shadow-sm">
+        <a href='{{ url('task') }}' class="btn btn-secondary">
+            << kembali</a>
+                <div class="mb-3 row">
+                    <label for="judul" class="col-sm-2 col-form-label">Judul</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" name='judul' value="{{ $data->judul }}" id="judul">
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="deskripsi" class="col-sm-2 col-form-label">Deskripsi</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" name='deskripsi' value="{{ $data->deskripsi }}" id="deskripsi">
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="status" class="col-sm-2 col-form-label">Status</label>
+                    <div class="col-sm-10">
+                        <select name="status" id="status">
+                            <option value="Belum Selesai" value="{{ $data->status }}">Belum Selesai</option>
+                            <option value="Selesai" value="{{ $data->status }}">Selesai</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="jurusan" class="col-sm-2 col-form-label"></label>
+                    <div class="col-sm-10"><button type="submit" class="btn btn-primary" name="submit">SIMPAN</button></div>
+                </div>
+    </div>
+</form>
+<!-- AKHIR FORM -->
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\taskController;
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/task/belumselesai', [taskController::class, 'indexBelumSelesai']);
+Route::get('/task/selesai', [taskController::class, 'indexSelesai']);
+Route::put('/task/{id}/status', [taskController::class, 'updateTaskStatus']);
+Route::get('/task/detail', [taskController::class, 'detail']);
+Route::resource('task', taskController::class);
+
